b723bc2b2bbb775d81c2cd6f1c784f277621eb34
pu17 <9134403+pu17@user.noreply.gitee.com>
2025-05-24 10:42:45 +0800

    add

--- finrl/meta/preprocessor/tusharedownloader.py
+++ finrl/meta/preprocessor/tusharedownloader.py
@@ -44,11 +44,7 @@ class TushareDownloader:
     def __init__(self, start_date: str, end_date: str, ticker_list: list):
         self.start_date = start_date
         self.end_date = end_date
-        self.ticker_list = ticker_list
-<<<<<<< HEAD
-=======
         self.ticker_list = self._add_stock_suffix(self.ticker_list)
->>>>>>> learning
         self.api_token = 'dcd771227705b0513e5a9cd4903ec0e0aedf182d7308af30afc3388d'
         ts.set_token(self.api_token)
         self.pro = ts.pro_api()
@@ -103,12 +99,7 @@ class TushareDownloader:
         # convert date to standard string format, easy to filter
 
         print("Shape of DataFrame: ", data_df.shape)
-<<<<<<< HEAD
         print(data_df.head())
-=======
-        print(data_df.tail())
->>>>>>> learning
-
         return data_df
     def get_all_a_stocks(self):
         """获取所有A股股票代码列表（上交所和深交所）
