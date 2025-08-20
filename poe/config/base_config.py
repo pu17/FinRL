f4db3a1bfb1b28ff1f0d7672a148322b7580dd0c
pu17 <359093268@qq.com>
2025-08-20 21:26:46 +0800

    一些简单的修改

--- poe/config/base_config.py
+++
@@ -1,9 +0,0 @@
-# config/base_config.py
-
-DATA_PATH = '/Users/pu17/Documents/stock/FinRL/poe/data/'
-
-MODEL_DIR = '/Users/pu17/Documents/stock/FinRL/poe/models/'
-
-BASE_PATH = '/Users/pu17/Documents/stock/FinRL/poe'
-
-LOG_LEVEL = 'INFO'
